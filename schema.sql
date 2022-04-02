ebd222e61b55a7e86e11f37087753d6af9d5816e
laramiedunlap <laramiedunlap@gmail.com>
2022-04-02 09:58:51 -0400

    reformatted cardholder names to first and last. created engine to sql db in jupyter with .env functionality

--- schema.sql
+++ schema.sql
@@ -2,58 +2,38 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/A0vAPe
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
-CREATE TABLE "CardHolder" (
-    "cardholder_ID" int   NOT NULL,
-    "first_name" VARCHAR(50)   NOT NULL,
-    "last_name" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_CardHolder" PRIMARY KEY (
-        "cardholder_ID"
-     )
+CREATE TABLE card_holder (
+	card_holder_id SERIAL NOT NULL, 
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	PRIMARY KEY (card_holder_id)
+	
 );
 
-CREATE TABLE "CreditCard" (
-    "card_number" VARCHAR(20)   NOT NULL,
-    "ch.CardHolderID" int   NOT NULL
+CREATE TABLE credit_card (
+	card_number VARCHAR(20),
+	card_holder_id int 
+
 );
 
-CREATE TABLE "MerchantCategory" (
-    "cat_ID" int   NOT NULL,
-    "merchant_type" str   NOT NULL,
-    CONSTRAINT "pk_MerchantCategory" PRIMARY KEY (
-        "cat_ID"
-     )
+CREATE TABLE merchant_category (
+	cat_id SERIAL NOT NULL,
+	merchant_type VARCHAR(50),
+	PRIMARY KEY (cat_id)
 );
 
-CREATE TABLE "Merchant" (
-    "merchant_ID" int   NOT NULL,
-    "merchant_name" str   NOT NULL,
-    "mc.CATID" int   NOT NULL,
-    CONSTRAINT "pk_Merchant" PRIMARY KEY (
-        "merchant_ID"
-     )
+CREATE TABLE merchant(
+	merchant_id SERIAL NOT NULL,
+	merchant_name VARCHAR(50),
+	cat_id int
+
 );
 
-CREATE TABLE "Transaction" (
-    "tran_ID" int   NOT NULL,
-    "date" timestamp   NOT NULL,
-    "amount" float   NOT NULL,
-    "card_number" VARCHAR(20)   NOT NULL,
-    "m.merchant_ID" int   NOT NULL,
-    CONSTRAINT "pk_Transaction" PRIMARY KEY (
-        "tran_ID"
-     )
+CREATE TABLE transactions (
+	transaction_id SERIAL NOT NULL,
+	date TIMESTAMP,
+	amount FLOAT,
+	card_number VARCHAR(20),
+	merchant_id int NOT NULL
+	
 );
-
-ALTER TABLE "CardHolder" ADD CONSTRAINT "fk_CardHolder_cardholder_ID" FOREIGN KEY("cardholder_ID")
-REFERENCES "CreditCard" ("ch.CardHolderID");
-
-ALTER TABLE "CreditCard" ADD CONSTRAINT "fk_CreditCard_ch.CardHolderID" FOREIGN KEY("ch.CardHolderID")
-REFERENCES "Transaction" ("card_number");
-
-ALTER TABLE "MerchantCategory" ADD CONSTRAINT "fk_MerchantCategory_cat_ID" FOREIGN KEY("cat_ID")
-REFERENCES "Merchant" ("mc.CATID");
-
-ALTER TABLE "Merchant" ADD CONSTRAINT "fk_Merchant_merchant_ID" FOREIGN KEY("merchant_ID")
-REFERENCES "Transaction" ("m.merchant_ID");
-
